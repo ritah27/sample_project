b55fbcc8d30e94fb8e5fb1b7fc1776ace2a12cd4
Ritah <ritahfaith27@gmail.com>
2023-10-18 05:41:57 -0700

    added code

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 console.log("Hello, world!");
 console.log("Hello, world!");
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+console.log("Hello, world!");
